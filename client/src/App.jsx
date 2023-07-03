69f442392d3a1fcdd04f9c4540ce2a9930606a54
Timothy <todongoonditi@gmail.com>
2023-07-04 02:42:17 +0300

    third commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from "./Pages/Home";
 import Layout from "./Layout/Layout";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import AboutUs from "./Pages/AboutUs";
 import ContactUs from "./Pages/ContactUs";
+import Home from "./Pages/Home";
 
 function App() {
     return (
         <BrowserRouter>
         <Routes>
         <Route path="/" element={<Layout />}>
-            <Route index element={<Home />}/>
-            <Route path="/Login" element={<Login />}/>
+            <Route index element={<Login />}/>
+            <Route path="/Home" element={<Home/>}/>
             <Route path="/Register" element={<Register />}/>
             <Route path="/AboutUs" element={< AboutUs/>}/>
             <Route path="/ContactUs" element={< ContactUs/>}/>
--- client/src/Layout/Navbar.jsx
+++ client/src/Layout/Navbar.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,27 +9,18 @@ function NavScrollExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="#">Real Estate</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Real Estate</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">About Us</Nav.Link>
-            
-            <Nav.Link href="#">
-              Our Contacts
-            </Nav.Link>
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+            <Nav.Link as={Link} to="/Home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/AboutUs">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/ContactUs">Our Contacts</Nav.Link>
           </Nav>
-          <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Log in</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Register
-              </NavDropdown.Item>           
-            </NavDropdown>
+          <NavDropdown title="have an account?" id="navbarScrollingDropdown">
+            <NavDropdown.Item as={Link} to="/Login">Log in</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/Register">Register</NavDropdown.Item>
+          </NavDropdown>
         </Navbar.Collapse>
       </Container>
     </Navbar>
