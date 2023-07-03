745c9b183b90a3052f7484b1d686f8625d277d97
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-07-04 01:09:28 +0300

    created components folder and home,about,contact files

---
+++ client/src/components/About.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+
+function About(){ 
+    return(
+        <div>
+            <h1>welcome to About us</h1>
+        </div>
+    )
+
+}
+export default About
---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+
+function Contact(){ 
+    return(
+        <div>
+            <h1>welcome to contact</h1>
+        </div>
+    )
+
+}
+export default Contact
