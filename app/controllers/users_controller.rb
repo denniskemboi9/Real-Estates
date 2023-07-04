13a3adee73c2890452013e93c73257673bfcd865
DennisKemboi <cherogonydennis9@gmail.com>
2023-07-04 07:59:18 +0300

    Migrations, Seed, and Basic Controller Actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-    def show
-        render json: @current_user
+    def index
+        render json: User.all
     end
 
     def create
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+        params.permit(:email, :)
     end
 
 
