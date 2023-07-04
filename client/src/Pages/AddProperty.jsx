3a2990888eadd16899da4a8d4258d9bfded9d83c
Timothy <todongoonditi@gmail.com>
2023-07-04 19:02:47 +0300

    created addproperty codes

--- client/src/Pages/AddProperty.jsx
+++ client/src/Pages/AddProperty.jsx
@@ -1,11 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function AddProperty() {
-    return (
-        <div>
-            Add Property
-        </div>
-    );
+  const [property, setProperty] = useState({
+    title: '',
+    description: '',
+    price: 0,
+    location: '',
+  });
+
+  const handleChange = (e) => {
+    setProperty({ ...property, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you can perform the logic to submit the property data to your backend or perform any other actions
+    console.log(property);
+    // Reset the form
+    setProperty({
+      title: '',
+      description: '',
+      price: 0,
+      location: '',
+    });
+  };
+
+  return (
+    <div className="container">
+      <h1>Add Property</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            name="title"
+            value={property.title}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="description">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            name="description"
+            value={property.description}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="price">
+          <Form.Label>Price</Form.Label>
+          <Form.Control
+            type="number"
+            name="price"
+            value={property.price}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="location">
+          <Form.Label>Location</Form.Label>
+          <Form.Control
+            type="text"
+            name="location"
+            value={property.location}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Add Property
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default AddProperty;
--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -12,14 +12,20 @@
 //   };
 
 //   return (
-//     <Form>
-//       <Form.Group className="mb-3" controlId="formBasicEmail">
+//     <div className='container'>
+//     <div id="sidebar" className="col-md hidden-xs hidden-sm">
+//       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxXAqp00vigLN_ub0joYrElLDkz3OYk7bQLkkbzmSxwRqWNIjmydNbWO9JDD9LwXR71b4&usqp=CAU" alt="" style={{ width: '400px', height: '400px' }} />
+//     </div>
+
+//       <div className='form'>
+//       <Form>
+//       <Form.Group className="col-md-4 col-sm-12 col-xs-12" controlId="formBasicEmail">
 //         <Form.Label>Email address</Form.Label>
 //         <Form.Control type="email" placeholder="Enter email" />
 //         <Form.Text className="text-muted"></Form.Text>
 //       </Form.Group>
 
-//       <Form.Group className="mb-3" controlId="formBasicPassword">
+//       <Form.Group className="col-md-4 col-sm-12 col-xs-12" controlId="formBasicPassword">
 //         <Form.Label>Password</Form.Label>
 //         <Form.Control type="password" placeholder="Password" />
 //       </Form.Group>
@@ -32,12 +38,19 @@
 //         Log in
 //       </Button>
 //     </Form>
+//       </div>
+//     </div> 
+//     <>
+//     <img src="https://static.wixstatic.com/media/4a7193_3d46254f0abd4093a0a93f8d6567d090~mv2.jpg/v1/fill/w_1251,h_678,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Enrogue%20Interior_CGI%20%20(3b).jpg" alt="" />
+//     <img src="" alt="" />
+//     <img src="" alt="" />
+//   </>
 //   );
 // }
 
 // export default Login;
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
@@ -49,27 +62,47 @@ function Login() {
   };
 
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted"></Form.Text>
-      </Form.Group>
+    <div className="container">
+      <div id="sidebar" className="col-md hidden-xs hidden-sm">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxXAqp00vigLN_ub0joYrElLDkz3OYk7bQLkkbzmSxwRqWNIjmydNbWO9JDD9LwXR71b4&usqp=CAU"
+          alt=""
+          style={{ width: '400px', height: '400px' }}
+        />
+      </div>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
+      <div className="form">
+        <Form>
+          <Form.Group className="col-md-4 col-sm-12 col-xs-12" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+          <Form.Group className="col-md-4 col-sm-12 col-xs-12" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
 
-      <Button variant="primary" type="button" onClick={handleLogin}>
-        Log in
-      </Button>
-    </Form>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+
+          <Button variant="primary" type="button" onClick={handleLogin}>
+            Log in
+          </Button>
+        </Form>
+      </div>
+
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV2raMEVn1tNeC0ozM-zPBadrH858ieCw_zw&usqp=CAU"
+        alt=""
+      />
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhWzqUIIvwXygiANznSwrB9ZdwYpKghzSr6g&usqp=CAU" alt="" />
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZrxKMod3gx6YRoVY4QUY9pH2isnr2QXPsA&usqp=CAU" alt="" />
+    </div>
   );
 }
 
 export default Login;
+
