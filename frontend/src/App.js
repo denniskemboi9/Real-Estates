4a44e42c00ca7a04d09175f6b49367a4f5f83e45
Timothy <todongoonditi@gmail.com>
2023-07-05 15:02:09 +0300

    updated navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,15 @@ import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import AddProperty from "./Pages/AddProperty";
 import ApprovePropert from "./Pages/ApproveProperty";
+import LandingPage from "./Pages/Landingpage";
 
 function App() {
     return (
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<Layout />}>
-            <Route index element={<Home />}/>
+            <Route index element={<LandingPage />}/>
+            <Route path="/Home" element={<Home />}/>
             <Route path="/Login" element={<Login />}/>
             <Route path="/Register" element={<Register />}/>
             <Route path="/AddProperty" element={<AddProperty />}/>
