396280862772b5325bab80c022d426d5b1361115
DennisKemboi <cherogonydennis9@gmail.com>
2023-07-10 10:05:07 +0300

    Deploying

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,8 @@ Rails.application.routes.draw do
 
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "*parts", to: "react#index"
 end
 
 
